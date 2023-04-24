b23ebf3d1bbeec6dd4059446ac8c7c7cc072d50e
yw4595 <106081987+yw4595@users.noreply.github.com>
2023-04-24 12:56:40 +0300

    Add files via upload

---
+++ JsonHtml/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using PeopleAppGlobals;
+using PeopleLib;
+using System.IO;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Net;
+
+namespace JsonHtml
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            Globals.AddPeopleSampleData();
+            List<Teacher> teachers = new List<Teacher>();
+            List<Student> students = new List<Student>();
+
+            foreach (KeyValuePair<string, Person> keyValuePair in Globals.people.sortedList)
+            {
+                if (keyValuePair.Value.GetType() == typeof(Teacher))
+                        {
+                            teachers.Add((Teacher)keyValuePair.Value);
+                        }
+                
+                
+                    else
+                {
+                    students.Add((Student)keyValuePair.Value);
+                }
+
+            }
+
+
+            string s = JsonConvert.SerializeObject(students);
+            string t = JsonConvert.SerializeObject(teachers);
+
+
+
+            StreamWriter writer = new StreamWriter("C:/Windows/Temp/students.json");
+            writer.Write(s);
+            writer.Close();
+
+
+            writer = new StreamWriter("C:/Windows/Temp/teachers.json");
+            writer.Write(t);
+            writer.Close();
+
+            StreamReader reader = new StreamReader("C:/Windows/Temp/students.json"); 
+            s = reader.ReadToEnd();
+            reader.Close();
+
+             reader = new StreamReader("C:/Windows/Temp/teachers.json");
+            t = reader.ReadToEnd();
+            reader.Close();
+
+
+            students = JsonConvert.DeserializeObject<List<Student>>(s);
+            teachers = JsonConvert.DeserializeObject<List<Teacher>>(t);
+
+            SortedList<string, Person> people = new SortedList<string, Person>();
+
+            foreach (Student student in students)
+            {
+                people[student.email] = student;
+            }
+            
+            foreach (Teacher teacher in teachers)
+            {
+                people[teacher.email] = teacher;
+            }
+
+            string url = "http://people.rit.edu/dxsigm/json.php";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse(); 
+            reader = new StreamReader(response.GetResponseStream());
+            t = reader.ReadToEnd();
+            reader.Close(); response.Close();
+
+            teachers = JsonConvert.DeserializeObject<List<Teacher>>(t);
+
+
+
+        }
+    }
+}
---
+++ TriviaApp/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json; // Importing Newtonsoft.Json for JSON serialization
+using System.IO;
+using System.Net;
+using System.Web;
+
+namespace TriviaApp
+{
+    // This class represents the result of a single trivia question
+    class TriviaResult
+    {
+        public string category; // The category of the trivia question
+        public string type; // The type of the trivia question (e.g. multiple choice)
+        public string difficulty; // The difficulty of the trivia question
+        public string question; // The question text
+        public string correct_answer; // The correct answer to the question
+        public List<string> incorrect_answers; // A list of incorrect answers
+    }
+
+    // This class represents the entire response from the trivia API
+    class Trivia
+    {
+        public int response_code; // A response code indicating success or failure
+        public List<TriviaResult> results; // A list of trivia results
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare variables
+            string url = null;
+            string s = null;
+            HttpWebRequest request;
+            HttpWebResponse response;
+            StreamReader reader;
+
+            // Set up the API URL to fetch a single random trivia question with multiple choices
+            url = "https://opentdb.com/api.php?amount=1&type=multiple";
+
+            // Send a HTTP GET request to the API endpoint
+            request = (HttpWebRequest)WebRequest.Create(url);
+            response = (HttpWebResponse)request.GetResponse();
+
+            // Read the response from the API endpoint
+            reader = new StreamReader(response.GetResponseStream());
+            s = reader.ReadToEnd();
+            reader.Close();
+
+            // Deserialize the JSON response from the API into a Trivia object
+            Trivia trivia = JsonConvert.DeserializeObject<Trivia>(s);
+
+            // Decode any HTML entities in the incorrect answer options
+            for (int i = 0; i < trivia.results[0].incorrect_answers.Count; ++i)
+            {
+                trivia.results[0].incorrect_answers[i] = HttpUtility.HtmlDecode(trivia.results[0].incorrect_answers[i]);
+            }
+        }
+    }
+}
